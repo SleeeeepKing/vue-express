eaa408ac387c0ff0c587aa5096e891be70a43296
Wenlei <wenlei.luo.ce@outlook.com>
2022-09-23 22:21:57 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# vue-express
-vue and express project
+# Vue-Express Project
+Group Member:
+
+Wenlei LUO, Yang WANG, Frédéric CHEN, Clément CHEN, Xin XU
+
+## Setup & Start & Build
+
+Node require 16.x
+
+Run yarn before starting code
+``` bash
+# install dependencies
+yarn
+
+# start server
+yarn run server
+
+# start website
+yarn dev
+```
---
+++ client/vite.config.ts
@@ -0,0 +1,38 @@
+import {defineConfig} from "vite";
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+
+export default defineConfig({
+    plugins: [ vue() ],
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: resolve(__dirname, './src'),
+            },
+            {
+                find: 'assets',
+                replacement: resolve(__dirname, '../src/assets'),
+            },
+            {
+                find: 'vue-i18n',
+                replacement: 'vue-i18n/dist/vue-i18n.cjs.js', // Resolve the i18n warning issue
+            },
+            {
+                find: 'vue',
+                replacement: 'vue/dist/vue.esm-bundler.js', // compile template
+            },
+        ],
+        extensions: ['.ts', '.js'],
+    },
+    server: {
+        proxy: {
+            '/api':{
+                target: 'http://localhost:8081',
+                changeOrigin: true,
+                rewrite: path => path.replace(/^\/api/,'')
+            }
+        }
+    }
+});
